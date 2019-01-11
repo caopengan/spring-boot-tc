68b4be80a6df7729cb6f930bbef739bf62540873
caopengan <291033240@qq.com>
2019-01-11 18:07:50 +0800

    spring-boot-tc 提交项目

--- src/main/java/com/tc/mapper/UserLoginMapper.java
+++ src/main/java/com/tc/mapper/UserLoginMapper.java
@@ -1,11 +1,17 @@
 package com.tc.mapper;
 
 import com.tc.entity.UserLogin;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
-@Mapper
 public interface UserLoginMapper {
+
+//    @Select("SELECT * FROM user_login")
+//    @Results({
+//            @Result(property = "userLoginId",  column = "user_login_id")
+//    })
     List<UserLogin> searchAll();
 }
